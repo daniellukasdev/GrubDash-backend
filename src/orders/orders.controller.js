3fe2a2d37979f1c8609b40aacb7ad86c71e1c38b
Daniel Lukas <dtlukasart@gmail.com>
2021-11-24 18:27:37 -0500

    fixed error object message and some refactoring

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,11 +6,52 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+// ##################  Request Functions  ##################
 
 function list(req, res) {
   res.json({ data: orders });
 }
+
+function read(req, res) {
+  res.json({ data: res.locals.order });
+}
+
+function create(req, res) {
+  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+function update(req, res) {
+  const foundOrder = res.locals.order;
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  const updatedOrder = {
+    id: foundOrder.id,
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes,
+  };
+  res.json({ data: updatedOrder });
+}
+
+function destory(req, res) {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === orderId);
+  if (index > -1) {
+    orders.splice(index, 1);
+  }
+  res.sendStatus(204);
+}
+
+// ##################  Middleware Functions  ##################
 
 function orderExists(req, res, next) {
   const { orderId } = req.params;
@@ -24,72 +65,6 @@ function orderExists(req, res, next) {
     status: 404,
     message: `Order id not found: ${req.params.orderId}`,
   });
-}
-
-function read(req, res) {
-  res.json({ data: res.locals.order });
-}
-
-function requiredFieldsCheck(data) {
-  const requiredFields = ["deliverTo", "mobileNumber", "dishes"];
-  for (const field of requiredFields) {
-    if (!data[field] && field !== "dishes") {
-      return {
-        status: 400,
-        message: `Order must include a ${field}`,
-      };
-    }
-    if (!data[field] && field === "dishes") {
-      return {
-        status: 400,
-        message: `Order must include a dish`,
-      };
-    }
-  }
-}
-
-function invalidDishQuantityCheck(dishes) {
-  for (const dish in dishes) {
-    if (
-      !dishes[dish].quantity ||
-      !Number.isInteger(dishes[dish].quantity) ||
-      dishes[dish].quantity <= 0
-    ) {
-      return {
-        status: 400,
-        message: `Dish ${dish} must have a quantity that is an integer greater than 0`,
-      };
-    }
-  }
-}
-
-function invalidDishesCheck(dishes) {
-  if (!Array.isArray(dishes) || dishes.length === 0) {
-    return {
-      status: 400,
-      messages: "Order must include at least one dish",
-    };
-  }
-  return invalidDishQuantityCheck(dishes);
-}
-
-function validateOrderId(id, orderId) {
-  if (orderId !== id) {
-    return {
-      status: 400,
-      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
-    };
-  }
-}
-
-function validateStatus(status) {
-  if (!status || status === "" || status === "invalid") {
-    return {
-      status: 400,
-      message:
-        "Order must have a status of pending, preparing, out-for-delivery, delivered",
-    };
-  }
 }
 
 function isValidOrder(req, res, next) {
@@ -116,21 +91,18 @@ function isValidOrder(req, res, next) {
     const invalidStatusError = validateStatus(data.status);
     if (invalidStatusError) return next(invalidStatusError);
   }
-
   next();
 }
 
-function create(req, res) {
-  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
-
-  const newOrder = {
-    id: nextId(),
-    deliverTo,
-    mobileNumber,
-    dishes,
-  };
-  orders.push(newOrder);
-  res.status(201).json({ data: newOrder });
+function verifyPendingStatus(req, res, next) {
+  const foundOrder = res.locals.order;
+  if (foundOrder.status !== "pending") {
+    return next({
+      status: 400,
+      message: "An order cannot be deleted unless it is pending",
+    });
+  }
+  next();
 }
 
 function checkIfDelivered(req, res, next) {
@@ -144,39 +116,68 @@ function checkIfDelivered(req, res, next) {
   next();
 }
 
-function update(req, res) {
-  const foundOrder = res.locals.order;
-  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+// ##################  Helper Functions  ##################
 
-  const updatedOrder = {
-    id: foundOrder.id,
-    deliverTo,
-    mobileNumber,
-    status,
-    dishes,
-  };
-  res.json({ data: updatedOrder });
+function requiredFieldsCheck(data) {
+  const requiredFields = ["deliverTo", "mobileNumber", "dishes"];
+  for (const field of requiredFields) {
+    if (!data[field] && field !== "dishes") {
+      return {
+        status: 400,
+        message: `Order must include a ${field}`,
+      };
+    }
+    if (!data[field] && field === "dishes") {
+      return {
+        status: 400,
+        message: `Order must include a dish`,
+      };
+    }
+  }
 }
 
-function verifyPendingStatus(req, res, next) {
-  const foundOrder = res.locals.order;
-  if (foundOrder.status !== "pending") {
-    return next({
+function invalidDishQuantityCheck(dishes) {
+  for (const dish in dishes) {
+    if (
+      !dishes[dish].quantity ||
+      !Number.isInteger(dishes[dish].quantity) ||
+      dishes[dish].quantity < 1
+    ) {
+      return {
+        status: 400,
+        message: `Dish ${dish} must have a quantity that is an integer greater than 0`,
+      };
+    }
+  }
+}
+
+function invalidDishesCheck(dishes) {
+  if (!Array.isArray(dishes) || dishes.length === 0) {
+    return {
       status: 400,
-      message: "An order cannot be deleted unless it is pending",
-    });
+      message: "Order must include at least one dish",
+    };
   }
-  
-  next();
+  return invalidDishQuantityCheck(dishes);
 }
 
-function destory(req, res) {
-  const { orderId } = req.params;
-  const index = orders.findIndex((order) => order.id === orderId);
-  if (index > -1) {
-    orders.splice(index, 1);
+function validateOrderId(id, orderId) {
+  if (orderId !== id) {
+    return {
+      status: 400,
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
+    };
   }
-  res.sendStatus(204);
+}
+
+function validateStatus(status) {
+  if (!status || status === "" || status === "invalid") {
+    return {
+      status: 400,
+      message:
+        "Order must have a status of pending, preparing, out-for-delivery, delivered",
+    };
+  }
 }
 
 module.exports = {
