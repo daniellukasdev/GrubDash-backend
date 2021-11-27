8cd744b703c41776ae498e60a8c4c9e8e1e88380
Daniel Lukas <dtlukasart@gmail.com>
2021-11-27 12:44:53 -0500

    added comments and organized controllers

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,40 +6,69 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+// ##################  Middleware Functions  ##################
 
-function list(req, res) {
-  res.json({ data: dishes });
-}
-
+/* checks that the dish with the ID from params exists */
 function dishExists(req, res, next) {
   const { dishId } = req.params;
   const foundDish = dishes.find((dish) => dish.id === dishId);
 
+  /* if the params ID matched an ID in the dishes array, 
+  assigns order to request locals and goes to the next function */
   if (foundDish) {
     res.locals.dish = foundDish;
     next();
   }
+  /* passes error object to error handler if no match is found */
   next({
     status: 404,
     message: `Dish id not found: ${req.params.dishId}`,
   });
 }
+/* invokes all necessary helper functions to make sure the dish is valid */
+function isValidDish(req, res, next) {
+  /* gets data object from request body */
+  const { data } = req.body;
+  /* checks to make sure data exists and returns error if it does not */
+  if (!data)
+    return next({
+      status: 400,
+      message: "Dish must have 'data' key.",
+    });
 
-function read(req, res) {
-  res.json({ data: res.locals.dish });
+  /* declares variable to store error object if the helper 
+  function returns an error object. If the error is returned,
+  the error object is passed to the error handler */
+  const invalidFieldsError = requiredFieldsCheck(data);
+  if (invalidFieldsError) {
+    return next(invalidFieldsError);
+  }
+
+  /* declares variable to store error object if the helper 
+  function returns an error object. If the error is returned,
+  the error object is passed to the error handler */
+  const invalidPriceError = invalidPriceCheck(data.price);
+  if (invalidPriceError) {
+    return next(invalidPriceError);
+  }
+
+  /* destructures the dishId from the request params. 
+  if it exists, it invokes the helper function to check that 
+  the ID is valid and returns an error if it is not. */
+  const { dishId } = req.params;
+  if (data.id) {
+    const invalidIdError = validateDishId(data.id, dishId);
+    if (invalidIdError) return next(invalidIdError);
+  }
+  // invokes the next function if all validations are successful
+  next();
 }
 
-// function hasDataProperty(req, res, next) {
-//   const { data } = req.body;
-//   if (!data)
-//     return next({
-//       status: 400,
-//       message: "Dish must have 'data' key.",
-//     });
-//   next();
-// }
+// ##################  Helper Functions  ##################
 
+/* function that takes in the data from the request body and checks 
+that it has all the required fields and returns an error object if it 
+does not */
 function requiredFieldsCheck(data) {
   const requiredFields = ["name", "description", "price", "image_url"];
   for (const field of requiredFields) {
@@ -52,6 +81,8 @@ function requiredFieldsCheck(data) {
   }
 }
 
+/* takes in the price and checks if it is a number and not less than 1
+returns an error object of not valid */
 function invalidPriceCheck(price) {
   if (typeof price !== "number" || price < 1) {
     return {
@@ -61,6 +92,7 @@ function invalidPriceCheck(price) {
   }
 }
 
+/* checks if the id property of the dish matches the id from the url params */
 function validateDishId(id, dishId) {
   if (dishId !== id) {
     return {
@@ -70,31 +102,19 @@ function validateDishId(id, dishId) {
   }
 }
 
-function isValidDish(req, res, next) {
-  const { data } = req.body;
-  if (!data)
-    return next({
-      status: 400,
-      message: "Dish must have 'data' key.",
-    });
+// ##################  Request Functions  ##################
 
-  const invalidFieldsError = requiredFieldsCheck(data);
-  if (invalidFieldsError) {
-    return next(invalidFieldsError);
-  }
-
-  const invalidPriceError = invalidPriceCheck(data.price);
-  if (invalidPriceError) {
-    return next(invalidPriceError);
-  }
-  const { dishId } = req.params;
-  if (data.id) {
-    const invalidIdError = validateDishId(data.id, dishId);
-    if (invalidIdError) return next(invalidIdError);
-  }
-  next();
+/* returns entire dishes array */
+function list(req, res) {
+  res.json({ data: dishes });
 }
 
+/* returns dish from response locals */
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+/* creates a new dish object */
 function create(req, res) {
   const { data: { name, description, price, image_url } = {} } = req.body;
 
@@ -110,6 +130,7 @@ function create(req, res) {
   res.status(201).json({ data: newDish });
 }
 
+/* updates an existing dish object */
 function update(req, res) {
   const foundDish = res.locals.dish;
   const { data: { name, description, price, image_url } = {} } = req.body;
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,67 +6,27 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// ##################  Request Functions  ##################
-
-function list(req, res) {
-  res.json({ data: orders });
-}
-
-function read(req, res) {
-  res.json({ data: res.locals.order });
-}
-
-function create(req, res) {
-  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
-  const newOrder = {
-    id: nextId(),
-    deliverTo,
-    mobileNumber,
-    dishes,
-  };
-  orders.push(newOrder);
-  res.status(201).json({ data: newOrder });
-}
-
-function update(req, res) {
-  const foundOrder = res.locals.order;
-  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
-
-  const updatedOrder = {
-    id: foundOrder.id,
-    deliverTo,
-    mobileNumber,
-    status,
-    dishes,
-  };
-  res.json({ data: updatedOrder });
-}
-
-function destory(req, res) {
-  const { orderId } = req.params;
-  const index = orders.findIndex((order) => order.id === orderId);
-  if (index > -1) {
-    orders.splice(index, 1);
-  }
-  res.sendStatus(204);
-}
-
 // ##################  Middleware Functions  ##################
 
+/* checks that the order with the ID from params exists */
 function orderExists(req, res, next) {
   const { orderId } = req.params;
   const foundOrder = orders.find((order) => order.id === orderId);
 
+  /* if the params ID matched an ID in the orders array, 
+  assigns order to request locals and goes to the next function */
   if (foundOrder) {
     res.locals.order = foundOrder;
     next();
   }
+  /* passes error object to error handler if no match is found */
   next({
     status: 404,
     message: `Order id not found: ${req.params.orderId}`,
   });
 }
 
+/* invokes all necessary helper functions to make sure the order is valid */
 function isValidOrder(req, res, next) {
   const { data } = req.body;
   if (!data)
@@ -75,14 +35,24 @@ function isValidOrder(req, res, next) {
       message: "Order must have 'data' key.",
     });
 
+  /* declares variable to store error object if the helper 
+  function returns an error object. If the error is returned,
+  the error object is passed to the error handler */
   const invalidFieldsError = requiredFieldsCheck(data);
   if (invalidFieldsError) {
     return next(invalidFieldsError);
   }
 
+  /* declares variable to store error object if the helper 
+  function returns an error object. If the error is returned,
+  the error object is passed to the error handler */
   const invalidDishesError = invalidDishesCheck(data.dishes);
   if (invalidDishesError) return next(invalidDishesError);
 
+  /* destructures the dishId from the request params. 
+  if it exists, it invokes the helper function to check that 
+  the ID and order status is valid and returns an error if 
+  either is not valid. */
   const { orderId } = req.params;
   if (data.id) {
     const invalidIdError = validateOrderId(data.id, orderId);
@@ -91,9 +61,11 @@ function isValidOrder(req, res, next) {
     const invalidStatusError = validateStatus(data.status);
     if (invalidStatusError) return next(invalidStatusError);
   }
+  // invokes the next function if all validations are successful
   next();
 }
 
+/* checks that order status if pending and returns an error if it is not */
 function verifyPendingStatus(req, res, next) {
   const foundOrder = res.locals.order;
   if (foundOrder.status !== "pending") {
@@ -102,9 +74,12 @@ function verifyPendingStatus(req, res, next) {
       message: "An order cannot be deleted unless it is pending",
     });
   }
+  // invokes the next function if all validations are successful
   next();
 }
 
+/* checks that the order status is not delivered and returns and 
+returns error if it is. */
 function checkIfDelivered(req, res, next) {
   const foundOrder = res.locals.order;
   if (foundOrder.status === "delivered") {
@@ -113,11 +88,15 @@ function checkIfDelivered(req, res, next) {
       message: "A delivered order cannot be changed",
     });
   }
+  // invokes the next function if all validations are successful
   next();
 }
 
 // ##################  Helper Functions  ##################
 
+/* function that takes in the data from the request body and checks 
+that it has all the required fields and returns an error object if it 
+does not */
 function requiredFieldsCheck(data) {
   const requiredFields = ["deliverTo", "mobileNumber", "dishes"];
   for (const field of requiredFields) {
@@ -127,6 +106,7 @@ function requiredFieldsCheck(data) {
         message: `Order must include a ${field}`,
       };
     }
+    /* if the dishes field does not exist, returns a different error */
     if (!data[field] && field === "dishes") {
       return {
         status: 400,
@@ -136,6 +116,7 @@ function requiredFieldsCheck(data) {
   }
 }
 
+/* checks that dish quantity is valid and returns error object if not */
 function invalidDishQuantityCheck(dishes) {
   for (const dish in dishes) {
     if (
@@ -151,6 +132,8 @@ function invalidDishQuantityCheck(dishes) {
   }
 }
 
+/* checks that dishes property is an array and not empty.
+Returns error if not */
 function invalidDishesCheck(dishes) {
   if (!Array.isArray(dishes) || dishes.length === 0) {
     return {
@@ -161,6 +144,7 @@ function invalidDishesCheck(dishes) {
   return invalidDishQuantityCheck(dishes);
 }
 
+/* checks if the id property of the order matches the id from the url params */
 function validateOrderId(id, orderId) {
   if (orderId !== id) {
     return {
@@ -170,6 +154,7 @@ function validateOrderId(id, orderId) {
   }
 }
 
+/* checks to order status to make sure it exists, is not empty, and is not invalid */
 function validateStatus(status) {
   if (!status || status === "" || status === "invalid") {
     return {
@@ -178,6 +163,56 @@ function validateStatus(status) {
         "Order must have a status of pending, preparing, out-for-delivery, delivered",
     };
   }
+}
+
+// ##################  Request Functions  ##################
+
+/*  */
+function list(req, res) {
+  res.json({ data: orders });
+}
+
+/*  */
+function read(req, res) {
+  res.json({ data: res.locals.order });
+}
+
+/*  */
+function create(req, res) {
+  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+/*  */
+function update(req, res) {
+  const foundOrder = res.locals.order;
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  const updatedOrder = {
+    id: foundOrder.id,
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes,
+  };
+  res.json({ data: updatedOrder });
+}
+
+/*  */
+function destory(req, res) {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === orderId);
+  if (index > -1) {
+    orders.splice(index, 1);
+  }
+  res.sendStatus(204);
 }
 
 module.exports = {
